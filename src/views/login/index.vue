d2207853744b247d314fb1bcd7038a32c87e2f29
steven-chang/kevinphone <kaiwen.chen@enmotech.com>
2022-07-14 18:47:54 +0800

    test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-container">
     <el-form class="login-form">
       <div class="title-container">
-        <h3 class="title">用户登录</h3>
+        <h3 class="title">登录</h3>
       </div>
       <el-form-item>
         <span class="svg-container">
